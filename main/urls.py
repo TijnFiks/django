fd8e7a3f370d06ced1df59638c6329e83622fc8c
TijnFiks <fiks.tijn@gmail.com>
2022-11-04 15:19:43 +0100

    Tweede versie

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index),
+    path('departments/', views.departments),
+    path('department/', views.new_department),
     path('employee/', views.new_employee)
 ]
--- main/views.py
+++ main/views.py
@@ -24,3 +24,14 @@ def new_employee(request):
     employee = Employee(id=id, first_name=first_name, last_name=last_name, department_id=department_id)
     employee.save()
     return redirect(index)
+
+def departments(request):
+    departments = Department.objects.all()
+    return render(request, 'departments.html', {'departments': departments})
+
+def new_department(request):
+    id = int(request.POST['id'])
+    department_name = request.POST['department_name']
+    department = Department(id=id, department_name=department_name)
+    department.save()
+    return redirect(departments)
